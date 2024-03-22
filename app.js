1ea23db9889f5bb45a8d3d1757be7797864c8974
ninap <paprocki.n@northeastern.edu>
2024-03-22 13:44:32 -0400

    A6 Mongo FA23

--- app.js
+++ app.js
@@ -12,11 +12,53 @@ import session from "express-session";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+// configure server session after cors
+// this is a default session configuration that works fine
+// locally, but needs to be tweaked further to work in a
+// remote server such as AWS, Render, or Heroku. See later
+
+import mongoose from "mongoose";
+import UserRoutes from "././Users/routes.js";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose
+  .connect(
+    "mongodb+srv://paprockin:Poprockz0009!!@kanbascluster.87ktvji.mongodb.net/?retryWrites=true&w=majority&appName=KanbasCluster",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.use(express.json());
 
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 
 Lab5(app);
 Hello(app);
---
+++ ~/.bash_profile
@@ -0,0 +1,1 @@
+DB_CONNECTION_STRING = 'mongodb+srv://paprockin:Poprockz0009!!@kanbascluster.87ktvji.mongodb.net/?retryWrites=true&w=majority&appName=KanbasCluster'
