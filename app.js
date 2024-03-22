4f11536fb8ee9547eba5adfbb4672fa6815a8d9c
ninap <paprocki.n@northeastern.edu>
2024-03-22 14:13:37 -0400

    Changing URL

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: "https://a6--magnificent-cascaron-573110.netlify.app",
   })
 );
 const sessionOptions = {
